2a38eed77415edeff768c054792201d69894c658
Jawhar <jawher.dhaou@esprit.tn>
2023-02-02 16:33:36 +0100

    Creation Entity ( TP )

---
+++ AM.ApplicationCore/Domain/Plane.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Domain
+{
+    public enum PlaneType
+    {
+        Boing,Airbus
+    }
+    public class Plane
+    {
+        public string Capacity { get; set; }
+        public DateTime ManualFactureDate { get; set; }
+        public int PlaneId { get; set; }
+        public PlaneType PlaneType { get; set; }
+    }
+}
