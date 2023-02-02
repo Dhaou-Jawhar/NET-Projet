4adc810f350cc4e596b5ceb948701bc34bdaafc8
Jawhar <jawher.dhaou@esprit.tn>
2023-02-02 14:48:40 +0100

    First Commit

---
+++ AM.ApplicationCore/Domain/Personne.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Domain
+{
+    public class Personne
+    {
+        int id;
+        string prenom;
+        string nom;
+        DateTime dateNaissance;
+        string email;
+        string password;
+        string confirmPassword;
+    }
+}
---
+++ AM.UI.Console/Program.cs
@@ -0,0 +1,41 @@
+﻿//// See https://aka.ms/new-console-template for more information
+////declaration variable 
+//string ch;
+////type: string, int, char, double, float, bool,DateTime, long, ....
+
+//for (int i = 0; i < 2; i++)
+//{
+//    ch = Console.ReadLine();
+
+//    Console.WriteLine("Bonjour " + ch);
+
+
+
+//    int chiffreValue = 0;
+
+//   // do
+//    //
+//        try
+//        {
+//            string chiffre = Console.ReadLine();
+//            chiffreValue = int.Parse(chiffre);
+//        if (chiffreValue > 15 && chiffreValue <= 18)
+//        { Console.WriteLine("Ados"); }
+//        else if (chiffreValue > 18)
+//        { Console.WriteLine("Adulte"); }
+
+//    }
+//        catch
+//        {
+//            Console.WriteLine("Erreur au niveau de conversion du chiffre");
+//        }
+//    //} while (chiffreValue == 0);
+
+//    Console.WriteLine("Votre nombre est " + (chiffreValue + 1));
+
+//}
+
+using AM.ApplicationCore.Domain;
+
+Personne p = new Personne();
+
