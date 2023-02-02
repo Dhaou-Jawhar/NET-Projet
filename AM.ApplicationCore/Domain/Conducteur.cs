a99bc29c02e95927d47143bf0a42f0329090c18a
Jawhar <jawher.dhaou@esprit.tn>
2023-02-02 15:52:56 +0100

    Methode parametrer

---
+++ AM.ApplicationCore/Domain/Conducteur.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Domain
+{
+    /*heritage c#  ( conducteur herite from personne ) */
+    internal class Conducteur : Personne
+    {
+        public DateTime DatePermis { get; set; }
+        public string TypePermis { get; set; }
+
+
+        /*base ( de personne ) + les proprités de Conducteur */
+        public override string ToString()
+        {
+            return base.ToString() + " "+DatePermis+" "+TypePermis;
+        }
+
+    }
+}
--- AM.ApplicationCore/Domain/Personne.cs
+++ AM.ApplicationCore/Domain/Personne.cs
@@ -9,13 +9,32 @@ namespace AM.ApplicationCore.Domain
     public class Personne
     {
 
-        
-
         /*constructeur sans parametre = ctor ( double tabulation) */
         public Personne()
         {
                 
         }
+
+        public bool login(string nom, string password) {
+
+            //var result = nom == nom || password == password ? true : false;
+            //return result;
+
+            //----------------------[OU]---------------------------
+
+            //if (nom==nom && password==password)
+            //    return true;
+            //else return false;
+
+            return nom == nom && password == password;
+        }
+
+        public bool login(string nom, string password, string email) { 
+        
+            return nom == nom && password == password && email == email;
+        }
+
+
         /*selectionner les proprités et ajouter un constructeur par la lompe jaune */
         public Personne(string nom, string prenom, DateTime dateNaissance, string email, string password, string confirmPassword)
         {
