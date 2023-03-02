d3f5a600fb9539ac8e7db67a5818dcbaaf9b0d6b
Jawhar <jawher.dhaou@esprit.tn>
2023-03-02 15:19:35 +0100

    rebase and update DB , AMContexte.cs

--- AM.Infrastructure/AMContexte.cs
+++ AM.Infrastructure/AMContexte.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,13 @@ namespace AM.Infrastructure
         {
             optionsBuilder.UseSqlServer(@"Data Source=(localDB)\MsSqlLocalDb; initial catalog = DhaouJawharNET ; Integrated Security = true");
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Flight>().HasKey(f => f.FlightId);
+            modelBuilder.Entity<Flight>().ToTable("MyFlight");
+            modelBuilder.Entity<Flight>().Property(j => j.Deparature).IsRequired().HasMaxLength(100).HasColumnName("ville de depart").HasDefaultValue("TOUNES").HasColumnType("nchar");
         }
 
 
